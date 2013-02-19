dfa36b1e72ba55d8bf969ec73ba1b190e9a9f838
Adam <adam@csuac.com>
2013-02-19 14:12:13 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CSUAC::Application.config.secret_token = 'b8ba5bcbf24b99a12f59a3b563558a25bc375141bdaaa10177ee5d9701d988e29336a0340048a7c3a14ccfb4d9e028cd84d3b3424e43ffaabdd97fabe37a58be'
